14ec6eae1faa4b5320bc38637405de962f2988bf
Charles Logston <charleslogston@gmail.com>
2017-03-17 10:39:51 -0700

    Revert "New foosbot skills: Rolling, Swap, Unofficial!"

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-## Slack Foosball Bot
+## Foos Bot
 
 # Using
 To use foos-bot, just provide an environment variable of `SLACK_API_TOKEN` with your slack api token.
-
-# Run
-npm start
-
-# Rolling stats
-npm run rolling
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Bot = require('slacky');
 const WebClient = require('@slack/client').WebClient;
 const whitelist = require('slacky-middleware-white-black-list').whitelist;
 const startListen = require('./lib/listeners/start');
-const unofficialListen = require('./lib/listeners/unofficial');
 const inListen = require('./lib/listeners/in');
 const stopListen = require('./lib/listeners/stop');
 const outListen = require('./lib/listeners/out');
@@ -10,35 +9,26 @@ const winListen = require('./lib/listeners/win');
 const helpListen = require('./lib/listeners/help');
 const statsListen = require('./lib/listeners/stats');
 const statListen = require('./lib/listeners/stat');
-const alltimeListen = require('./lib/listeners/alltime');
-const fieldListen = require('./lib/listeners/field');
-const swapListen = require('./lib/listeners/swap');
-const token = process.env.SLACK_API_TOKEN;
+const token = process.env.SLACK_API_TOKEN || '';
 
 module.exports = bot = new Bot(token);
 bot.web = new WebClient(token);
 
-bot.use(whitelist(['foos', 'foosbottest'], bot.web));
+bot.use(whitelist(['foos', 'mikestest'], bot.web));
 bot.currentGames = {};
 bot.maximum = 4;
 bot.timeout = 10 * 60 * 1000;
-bot.swapCnt = 0;
-bot.unofficial = false;
 
 // Add listeners
 [
   startListen,
-  unofficialListen,
   inListen,
   outListen,
   stopListen,
   winListen,
   helpListen,
   statListen,
-  statsListen,
-  alltimeListen,
-  fieldListen,
-  swapListen
+  statsListen
 ].forEach((listener) => {
   bot.listen(listener.matcher, listener.callback);
 })
